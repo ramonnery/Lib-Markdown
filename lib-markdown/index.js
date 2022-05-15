b519e45a6b2d7abd4c4376cb7fd9e8b8c4a32e06
Ramon Nery <76761298+ramonnery@users.noreply.github.com>
2022-05-15 16:54:27 -0300

    Enviando lib markdown

---
+++ lib-markdown/index.js
@@ -0,0 +1,42 @@
+import fs from 'fs'
+
+function extraiLinks(texto) {
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm
+    let arrayResultados = []
+    let temp;
+
+    while((temp = regex.exec(texto)) !== null) {
+        arrayResultados.push({ [temp[1]]: temp[2] })
+    }
+
+    return arrayResultados.length === 0 ? 'não há links' : arrayResultados
+}   
+
+
+function trataErro(erro) {
+    throw new Error(chalk.red.bold(erro.code, 'Não há arquivo o caminho'))
+}
+
+
+async function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8'
+    try {
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        return extraiLinks(texto)
+    }
+    catch(erro) {
+        trataErro(erro)
+    }
+}
+
+export { pegaArquivo }
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8'
+//     fs.promises
+//     .readFile(caminhoDoArquivo, encoding)
+//     .then((texto) => console.log(chalk.green(texto)))
+//     .catch((erro) => trataErro(erro))
+// }
+
+// pegaArquivo('./arquivos/texto1.md')
